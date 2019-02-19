d6102ede6eef33a3ec08bbbd15af2c9363b7fc41
Sachin Chand <sachinchand20@gmail.com>
2019-02-19 13:36:43 +0530

    Genesis block and hash block added

--- block.js
+++ block.js
@@ -1,3 +1,5 @@
+const SHA256 = require('crypto-js/sha256');
+
 class Block{
     constructor(timestamp,lastHash,hash,data)
     {
@@ -14,6 +16,32 @@ class Block{
                  hash     :${this.hash.substring(0,10)}
                  data     :${this.data}`
       
+    }
+
+    //genesis funrtion
+    static genesis(){
+        return new this('Genesis time','-----','f!r57-h45h',[]); 
+    }
+
+//mining the last block
+    static mineBlock(lastBlock,data){
+        const timestamp = Date.now();
+        const lastHash = lastBlock.hash;
+        const hash = Block.hash(timestamp, lastHash, data);
+        
+        return new this(timestamp, lastHash, hash, data);
+    }
+    
+
+    static hash(timestamp, lastHash,  data){
+        return SHA256('${timestamp}${lasthash}${data}').toString();
+
+    }
+
+
+    static blockHash(block) {
+        const { timestamp, lastHash, data } = block;
+      return Block.hash(timestamp, lastHash, data);
     }
 }
 console.log('Inside block chian');
--- dev-test.js
+++ dev-test.js
@@ -1,5 +1,10 @@
 const Block=require('./block.js');
-const block=new Block('foo','bar','zoo','baz');
-console.log("Hello world"+block.toString());
 
+// const block=new Block('foo','bar','zoo','baz');
+
+// console.log("Hello world"+block.toString());
+// console.log(Block.genesis().toString());
+
+const fooBlock = Block.mineBlock(Block.genesis(), 'foo');
+console.log(fooBlock.toString());
 
